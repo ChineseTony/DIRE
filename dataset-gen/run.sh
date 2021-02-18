fce2576ffba9392880f3e183c4c94cda6afa5f9d
“ChineseTony“ <13206573871@163.com>
2021-02-18 18:23:47 +0800

    add run script

---
+++ dataset-gen/run.sh
@@ -0,0 +1,5 @@
+python3 run_decompiler.py --ida '/home/tom/Desktop/idapro-7.5/idat64' '/home/tom/Desktop/input' '/home/tom/Desktop/files'
+
+cd /home/tom/Desktop/
+tar -zcvf  files.tar.gz files
+#mv test.tar.gz /home/tom/Desktop/DIRE/neural-model/data/preprocessed_data/
